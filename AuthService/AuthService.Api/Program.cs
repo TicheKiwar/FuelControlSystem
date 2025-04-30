cc03682bdd43476b1f7519a71b62fbbec9fe0276
TicheKiwar <kiwartiche@gmail.com>
2025-04-30 07:37:26 -0500

    Added dependencies for mongodb database

---
+++ AuthService/AuthService.Api/Program.cs
@@ -0,0 +1,4 @@
+using Common.Common.Api;
+
+var app = DefaultDistribWeb.CreateWebAplication();
+DefaultDistribWeb.RunWebAplication(app);
---
+++ Common/Common.Api/DefaultDistribWeb.cs
@@ -0,0 +1,34 @@
+﻿namespace Common.Common.Api
+{
+    public static class DefaultDistribWeb
+    {
+        public static WebApplication CreateWebAplication(Action<WebApplicationBuilder>? webAppBuilder = null)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder();
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            if (webAppBuilder != null)
+            {
+                webAppBuilder.Invoke(builder);
+            }
+
+            return builder.Build();
+        } 
+
+    public static void RunWebAplication(WebApplication webApplication)
+        {
+            // Configure the HTTP request pipeline.
+            if (webApplication.Environment.IsDevelopment())
+            {
+                webApplication.UseSwagger();
+                webApplication.UseSwaggerUI();
+            }
+            webApplication.UseHttpsRedirection();
+            webApplication.UseAuthorization();
+            webApplication.MapControllers();
+            webApplication.Run();
+        }
+    }
+}
