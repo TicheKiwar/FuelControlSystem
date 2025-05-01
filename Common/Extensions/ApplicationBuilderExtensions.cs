c6901ceb639f40b6c96a8a536d207883663595af
TicheKiwar <kiwartiche@gmail.com>
2025-05-01 11:13:03 -0500

    Middleware shared for all microservices

---
+++ Common/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Common.Middlewares;
+
+namespace Common.Extensions;
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseJwtMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<JwtMiddleware>();
+    }
+}
